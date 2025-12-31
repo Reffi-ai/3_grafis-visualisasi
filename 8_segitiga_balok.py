7e5eed0ccf35d984d55f9ce5dcc96d7ec4640787
Reffi Rizqullah <reffirizqullah@gmail.com>
2025-12-31 17:08:36 +0700

    feat: add rotating cube

--- 8_segitiga_balok.py
+++ 8_segitiga_balok.py
@@ -13,9 +13,9 @@ class Test(Base):
         print("Menginisialisasi program untuk menampilkan segitiga dan kotak sederhana...")
         self.renderer = Renderer()
         self.scene = Scene()
-        self.scene.setPosition([0.0, 0.0, 0.0]) 
+        self.scene.setPosition([0.0, 4.0, 0]) 
         self.camera = Camera( aspectRatio=800/600 )
-        self.camera.setPosition( [0, 0, 4] ) # Kamera di [0,0,4] melihat ke origin
+        self.camera.setPosition( [0, 4, 4] ) # Kamera di [0,0,4] melihat ke origin
 
         # --- OBJEK 1: SEGITIGA ---
         # Anda bisa mengubah koordinat X, Y, Z dari setiap verteks di bawah ini.
@@ -76,6 +76,7 @@ class Test(Base):
         # Coba aktifkan baris di bawah ini satu per satu untuk melihat efeknya:
         # self.triangleMesh.rotateX(np.pi / 4) # Memutar segitiga
         # self.boxMesh.scale(0.7)             # Memperkecil kotak
+        # self.boxMesh.rotateX(np.pi / 4) 
 
     def update(self):
         # Tidak ada rotasi otomatis di sini, balok akan diam.
